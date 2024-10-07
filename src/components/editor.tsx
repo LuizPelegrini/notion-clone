4086a4120565bf188c0d0d6a3c9fa54cec9cfa3d
LuizPelegrini <luiz.guilherme@stashaway.com>
2024-10-07 21:44:59 +0800

    add bold and italic menu

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,10 +1,13 @@
 import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight';
-import { EditorContent, useEditor } from '@tiptap/react';
+import { BubbleMenu, EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import html from 'highlight.js/lib/languages/xml';
 import { createLowlight } from 'lowlight';
 import content from '../initial-content.json';
 import 'highlight.js/styles/github-dark.min.css';
+import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
+import { cn } from '@/lib/utils';
+import { FontBoldIcon, FontItalicIcon } from '@radix-ui/react-icons';
 
 const lowlight = createLowlight();
 lowlight.register('html', html);
@@ -25,5 +28,41 @@ export const Editor = () => {
 		},
 	});
 
-	return <EditorContent editor={editor} />;
+	if (!editor) {
+		return null;
+	}
+
+	return (
+		<>
+			<EditorContent editor={editor} />
+			<BubbleMenu editor={editor}>
+				<ToggleGroup
+					type="multiple"
+					size="sm"
+					className="shadow-center bg-white p-1 rounded-xl"
+				>
+					<ToggleGroupItem
+						value="bold"
+						onClick={() => editor.chain().focus().toggleBold().run()}
+						className={cn('w-8 h-8 p-0', {
+							'text-blue-400': editor.isActive('bold'),
+						})}
+						data-state="off"
+					>
+						<FontBoldIcon width={16} height={16} />
+					</ToggleGroupItem>
+					<ToggleGroupItem
+						value="italic"
+						onClick={() => editor.chain().focus().toggleItalic().run()}
+						className={cn('w-8 h-8 p-0', {
+							'text-blue-400': editor.isActive('italic'),
+						})}
+						data-state="off"
+					>
+						<FontItalicIcon />
+					</ToggleGroupItem>
+				</ToggleGroup>
+			</BubbleMenu>
+		</>
+	);
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+	darkMode: ['class'],
 	content: ['./src/**/index.html', './src/**/*.{ts,tsx,js,jsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)',
+			},
+			boxShadow: {
+				center: '0 0 14px 0px rgb(0 0 0 / 0.1)',
+			},
+			colors: {},
+		},
 	},
-	plugins: [require('@tailwindcss/typography')],
+	plugins: [require('@tailwindcss/typography'), require('tailwindcss-animate')],
 };
